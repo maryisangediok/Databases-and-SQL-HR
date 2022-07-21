c83665a8ab14b76ed1ccdddf8513580492dfdf89
Mary Isangediok <67558383+maryisangediok@users.noreply.github.com>
2022-07-21 10:15:35 -0500

    Update SubQueries_Script.sql

--- SubQueries_Script.sql
+++ SubQueries_Script.sql
@@ -1,25 +1,25 @@
 
 --- Query 1 ---
-select * 
-from employees 
-where salary < AVG(salary)
+SELECT * 
+FROM EMPLOYEES 
+WHERE salary < AVG(SALARY)
 ;
 --- Query 2---	
-select EMP_ID, F_NAME, L_NAME, SALARY 
-from employees 
-where SALARY < 
-	(select AVG(SALARY) 
-	from employees)
+SELECT EMP_ID, F_NAME, L_NAME, SALARY 
+FROM EMPLOYEES 
+WHERE SALARY < 
+	(SELECT AVG(SALARY) 
+	FROM EMPLOYEES)
 ;
 --- Query 3 ---
-select EMP_ID, SALARY, MAX(SALARY) AS MAX_SALARY 
-from employees
+SELECT EMP_ID, SALARY, MAX(SALARY) AS MAX_SALARY 
+FROM EMPLOYEES
 ;	
 --- Query 4 ---
-select EMP_ID, SALARY, ( select MAX(SALARY) from employees ) AS MAX_SALARY 
-from employees
+SELECT EMP_ID, SALARY, (SELECT MAX(SALARY) FROM EMPLOYEES ) AS MAX_SALARY 
+FROM EMPLOYEES
 ;
 --- Query 5 ---
-select * 
-from ( select EMP_ID, F_NAME, L_NAME, DEP_ID from employees) AS EMP4ALL 
+SELECT * 
+FROM (SELECT EMP_ID, F_NAME, L_NAME, JOB_ID, MANAGER_ID, DEP_ID FROM EMPLOYEES) AS EMP4ALL 
 ;
